dc0abd51080028ace751fc06be7922d775fae4d3
Quincy Sesay <98617165+quincysesay@users.noreply.github.com>
2022-03-18 13:21:18 +0000

    Delete testing_bites directory

--- testing_bites/testing_bites/spec/greet_spec.rb
+++
@@ -1,8 +0,0 @@
-require "greet"
-
-RSpec.describe "greet method" do
-  it "it prints Hello + user_name" do
-    result = greet("Quincy")
-    expect(result).to eq "Hello, Quincy!"
-  end
-end
--- testing_bites/testing_bites/spec/report_length_spec.rb
+++
@@ -1,8 +0,0 @@
-require "report_length"
-
-RSpec.describe "report_length method" do
-  it "returns the length of a string input" do
-  result = report_length("hello")
-  expect(result).to eq "This string was 5 characters long."
-  end
-end
